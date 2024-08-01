edaa940422f5ae55c322678c912d55953bc48296
Adrian Opsahl <adrian.opsahl@gmail.com>
2024-08-01 10:11:37 +0200

    fixed correct sql and renamed routes

--- src/turso.ts
+++ src/turso.ts
@@ -4,4 +4,5 @@ export const turso = createClient({
     url: ":memory:",
 });
 
-turso.execute("CREATE TABLE IF NOT EXISTS counter (counter INTEGER NOT NULL DEFAULT 0);");
+await turso.execute("CREATE TABLE IF NOT EXISTS counters (id INTEGER PRIMARY KEY ASC, counter INTEGER NOT NULL DEFAULT 0);");
+await turso.execute("INSERT INTO counters (counter) VALUES (0);");
